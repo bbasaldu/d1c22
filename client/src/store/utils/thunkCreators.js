c4f67989f3c66a85024918ea3cf7a4baa8bdf1d6
Brian <bbasaldu@ucsc.edu>
2022-01-11 18:45:31 -0800

    fixed new messages not showing up

--- client/src/store/utils/thunkCreators.js
+++ client/src/store/utils/thunkCreators.js
@@ -72,6 +72,7 @@ export const logout = (id) => async (dispatch) => {
 export const fetchConversations = () => async (dispatch) => {
   try {
     const { data } = await axios.get("/api/conversations");
+    console.log(data)
     dispatch(gotConversations(data));
   } catch (error) {
     console.error(error);
@@ -93,10 +94,11 @@ const sendMessage = (data, body) => {
 
 // message format to send: {recipientId, text, conversationId}
 // conversationId will be set to null if its a brand new conversation
-export const postMessage = (body) => (dispatch) => {
-  try {
-    const data = saveMessage(body);
+export const postMessage = (body) => async (dispatch) => {
 
+  try {
+    const data = await saveMessage(body);
+    
     if (!body.conversationId) {
       dispatch(addConversation(body.recipientId, data.message));
     } else {
